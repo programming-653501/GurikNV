be2b0980014c27767ad282c89ad0f9c07a4dfd97
Nikita <gurritoglebon@gmail.com>
2017-02-19 00:52:55 +0300

    Lab 1.2(6)

---
+++ Prog1/Prog1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double da;
+        WriteLine("Введите число:");
+        while (!double.TryParse(ReadLine(), out da))
+        {
+            WriteLine("Некорректный ввод!");
+        }
+        var ia = (int) Math.Truncate((da - Math.Truncate(da)) * 1000);
+        WriteLine("{0}", (ia % 10) + ((ia / 10) % 10) + (ia / 100));
+        ReadKey();
+    }
+}
